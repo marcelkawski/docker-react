d9bdc6961de5a1f7acb1456916ed17ad8af62c36
marcelkawski <marcel.kawski1@gmail.com>
2023-03-28 12:54:36 +0300

    Revert changing app text

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          I was changed on feature branch.
+          Learn React
         </a>
       </header>
     </div>
